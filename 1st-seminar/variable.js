aaed92f275d24274dbe1c3a8791fd38b298cba30
seonuk <sunwook317@gmail.com>
2020-10-10 21:22:50 +0900

    first commit

---
+++ 1st-seminar/variable.js
@@ -0,0 +1,15 @@
+var variableVar = "123";
+var variableVar = "321";
+console.log(`variableVar: ${variableVar}`);
+
+let variableLet = "123";
+let variableLet = "321";
+console.log(`variableLet: ${variableLet}`);
+
+const variableConst = "123";
+const variableConst = "321";
+console.log(`variableConst: ${variableConst}`);
+
+const objectConst = { name: "최선욱" };
+objectConst.name = "홍길동";
+console.log(objectConst);
